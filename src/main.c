cd7847737b4df9feb883e567b82f2dcf291f36ed
extace <extace>
2003-05-25 22:54:32 +0000

    Commented out loading of RC file as it conflicts with GTK+2's theme engine and makes the app not look good...

--- src/main.c
+++ src/main.c
@@ -29,8 +29,6 @@ gint def_comm_port;
 int main(int argc, char ** argv)
 {
 	gint cfg_result;
-	GtkBindingSet *binding_set;
-
 
 	g_thread_init(NULL);
 	gdk_threads_init();
@@ -38,28 +36,9 @@ int main(int argc, char ** argv)
 	gtk_init(&argc, &argv);
 
 	gtk_set_locale();
-	/* Check to see if we are being run from the correct
-	 * directory.
-	 */
-	if (file_exists("MegaTunixrc"))
-		gtk_rc_add_default_file ("MegaTunixrc");
 
-	binding_set = gtk_binding_set_by_class (gtk_type_class (GTK_TYPE_WIDGET));
-	gtk_binding_entry_add_signal (binding_set,
-			'9', GDK_CONTROL_MASK | GDK_RELEASE_MASK,
-			"debug_msg",
-			1,
-			G_TYPE_STRING, "GtkWidgetClass <ctrl><release>9 test");
-
-	/* We use gtk_rc_parse_string() here so we can make sure it works across theme
-	 * changes
-	 */
-
-	gtk_rc_parse_string ("style \"testgtk-version-label\" { "
-			"   fg[NORMAL] = \"#ff0000\"\n"
-			"   font = \"Sans 18\"\n"
-			"}\n"
-			"widget \"*.testgtk-version-label\" style \"testgtk-version-label\"");
+//	if (file_exists("MegaTunixrc"))
+//		gtk_rc_add_default_file ("MegaTunixrc");
 
 	init();			/* initialize global vars */
 	mem_alloc();
