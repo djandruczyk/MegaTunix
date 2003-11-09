807b83cb186fd24d403854f5b13d1602d28152af
extace <extace>
2003-11-09 04:53:44 +0000

    Forgot to add vex_support.{ch}

---
+++ include/vex_support.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#ifndef __VEX_SUPPORT_H__
+#define __VEX_SUPPORT_H__
+
+#include <gtk/gtk.h>
+
+/* Prototypes */
+void vetable_export(void);
+void vetable_import(void);
+/* Prototypes */
+
+#endif
---
+++ src/vex_support.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#include <config.h>
+#include <defines.h>
+#include <enums.h>
+#include <globals.h>
+#include <stdio.h>
+#include <structures.h>
+#include <vex_support.h>
+
+//extern struct  
+
+
+void vetable_export()
+{
+	printf("export VEtable\n");
+}
+
+void vetable_import()
+{
+	printf("import VEtable\n");
+}
