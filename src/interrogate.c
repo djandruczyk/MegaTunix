668be70d8a9f3d63c9e60f3165e2029bfc4c6e01
extace <extace>
2004-07-19 17:51:23 +0000

    Added interrogator lines for the MSnS modified firmware with all sorts of new features by Phil Ringwood. (daxtojeiro). Fixed an issue during interrogation where if interrogation failed it never set the table sizes to sane values leading to scrambled data everywhere...  Set sane defaults of 22 (rt) and 128 (VE)

--- src/interrogate.c
+++ src/interrogate.c
@@ -41,7 +41,7 @@ gboolean interrogated = FALSE;
 
 static struct Canidate
 {
-	gint bytes[10];		/* byte count for each of the 9 test cmds */
+	gint bytes[11];		/* byte count for each of the 11 test cmds */
 	unsigned char *v0_data;	/* V0 data */
 	unsigned char *v1_data;	/* V1 data */
 	gchar *sig_str;		/* Signature string to search for */
@@ -51,32 +51,38 @@ static struct Canidate
 	gint capabilities;	/* Bitmask of capabilities.... */
 } canidates[] = 
 {
-	{ {22,0,0,125,125,0,0,0,0,0},NULL,NULL,NULL,NULL,0,
+	{ {22,0,0,125,125,0,0,0,0,0,0},NULL,NULL,NULL,NULL,0,
 			"Old Bowling & Grippo 1.0\0",STD},
-	{ {22,1,1,125,125,0,0,0,0,0},NULL,NULL,NULL,NULL,20,
+	{ {22,1,1,125,125,0,0,0,0,0,0},NULL,NULL,NULL,NULL,20,
 			"Standard Bowling & Grippo (2.0-3.01)\0",
 			STD},
-	{ {22,1,1,128,128,0,0,0,255,255}, NULL,NULL,NULL,NULL,1,
+	{ {22,1,1,128,128,0,0,0,0,255,255}, NULL,NULL,NULL,NULL,1,
 			"Dualtable 0.90-1.0\0",DUALTABLE},
-	{ {22,1,1,128,128,18,0,0,255,255},NULL,NULL,"v.1.01\0",NULL,1,
+	{ {22,1,1,128,128,18,0,0,0,255,255},NULL,NULL,"v.1.01\0",NULL,1,
 			"Dualtable 1.01\0",DUALTABLE},
-	{ {22,1,1,128,128,19,0,0,255,255},NULL,NULL,"v.1.02\0",NULL,1,
+	{ {22,1,1,128,128,19,0,0,0,255,255},NULL,NULL,"v.1.02\0",NULL,1,
 			"Dualtable 1.02\0",DUALTABLE|IAC_PWM},
-	{ {22,1,1,128,128,17,0,0,0,0},NULL,NULL,"Rover IAC\0",NULL,30,
+	{ {22,1,1,128,128,17,0,0,0,0,0},NULL,NULL,"Rover IAC\0",NULL,30,
 			"MS-3.0 Rover IAC (3.0.4)\0",IAC_STEPPER},
-	{ {22,1,1,128,128,16,0,0,0,0},NULL,NULL,"Rover IAC\0",NULL,30,
+	{ {22,1,1,128,128,16,0,0,0,0,0},NULL,NULL,"Rover IAC\0",NULL,30,
 			"MS-3.0 Rover IAC (3.0.5)\0",IAC_STEPPER},
-	{ {22,1,1,125,125,0,0,83,0,0},NULL,NULL,NULL,NULL,20,
+	{ {22,1,1,125,125,0,0,83,0,0,0},NULL,NULL,NULL,NULL,20,
 			"MegaSquirtnEDIS v0.108 OR SquirtnSpark 2.02\0",
 			S_N_EDIS},
-	{ {22,1,1,125,125,0,0,95,0,0},NULL,NULL,NULL,NULL,30,
+	{ {22,1,1,125,125,0,0,95,0,0,0},NULL,NULL,NULL,NULL,30,
 			"SquirtnSpark 3.0\0",S_N_SPARK},
-	{ {22,1,1,125,125,0,32,95,0,0},NULL,NULL,NULL,"EDIS v3.005\0",30,
+	{ {22,1,1,125,125,0,32,95,0,0,0},NULL,NULL,NULL,"EDIS v3.005\0",30,
 			"MegaSquirtnEDIS 3.005\0",S_N_EDIS},
-	{ {22,1,1,125,125,0,32,95,0,0},NULL,NULL,NULL,"EDIS v3.007\0",30,
+	{ {22,1,1,125,125,0,32,95,0,0,0},NULL,NULL,NULL,"EDIS v3.007\0",30,
 			"MegaSquirtnEDIS 3.007\0",S_N_EDIS},
-	{ {22,1,1,125,125,0,0,99,255,255},NULL,NULL,NULL,NULL,13,
+	{ {22,1,1,125,125,0,0,99,0,255,255},NULL,NULL,NULL,NULL,13,
 			"MegaSquirt'N'Spark Extended 3.0.1\0",
+			S_N_SPARK|LAUNCH_CTRL},
+	{ {26,1,1,128,128,38,0,95,254,0,0},NULL,NULL,"MBJ - Mod-e\0",NULL,30,
+			"** V3.01 Embedded Code by MBJ - Mod-e\0",
+			S_N_SPARK|LAUNCH_CTRL},
+	{ {26,1,1,128,128,41,0,128,255,0,0},NULL,NULL,"MBJ - Mod-e\0",NULL,30,
+			"** V3.01 Embedded Code by MBJ - Mod-e1**\0",
 			S_N_SPARK|LAUNCH_CTRL}
 };
 
@@ -96,6 +102,7 @@ static struct
 	{ 0,"S", "Signature Echo", 1, FALSE },
 	{ 0,"?", "Extended Version", 1, FALSE },
 	{ 0,"I", "Ignition Vars", 1, FALSE },
+	{ 0,"Y", "Big Block of Vars", 1, FALSE },
 	{ 0,"F0", "Memory readback 1st 256 bytes", 2, FALSE },
 	{ 0,"F1", "Memory readback 2nd 256 bytes", 2, FALSE }
 };
@@ -110,6 +117,7 @@ typedef enum
 	CMD_S,
 	CMD_QUEST,
 	CMD_I,
+	CMD_Y,
 	CMD_F0,
 	CMD_F1
 }TestCmds;
@@ -219,11 +227,9 @@ void interrogate_ecu()
 					canidate->v1_data = g_memdup(buf,total);
 					break;
 				case CMD_S:
-//					strncpy(canidate->sig_str,buf,total);
 					canidate->sig_str = g_strndup(buf,total);
 					break;
 				case CMD_QUEST:
-					//strncpy(canidate->quest_str,buf,total);
 					canidate->quest_str = g_strndup(buf,total);
 					break;
 				case CMD_Q:
@@ -378,12 +384,22 @@ void determine_ecu(void *ptr)
 	dbg_func(tmpbuf,INTERROGATOR);
 	update_logbar(interr_view,"warning",tmpbuf,FALSE,FALSE);
 	g_free(tmpbuf);
+	goto freeup;
+
 
 cleanup:
+	serial_params->table0_size = 128;  /* assumptions!!!! */
+	serial_params->rtvars_size = 22;  /* assumptions!!!! */
+
+freeup:
 	if (canidate->v0_data)
 		g_free(canidate->v0_data);
 	if (canidate->v1_data)
 		g_free(canidate->v1_data);
+	if (canidate->sig_str)
+		g_free(canidate->sig_str);
+	if (canidate->quest_str)
+		g_free(canidate->quest_str);
 	return;
 
 }
