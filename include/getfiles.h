e0aa405355b262820ebbad8068f171e510d643e5
extace <extace>
2004-11-07 19:37:35 +0000

    Added getfiles.{ch}. (stubs for now,  will add content soon)

---
+++ include/getfiles.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#ifndef __GETFILES_H__
+#define __GETFILES_H__
+
+#include <gtk/gtk.h>
+
+/* Prototypes */
+gchar ** get_files(gchar * );
+/* Prototypes */
+
+#endif
---
+++ src/getfiles.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#include <config.h>
+#include <defines.h>
+#include <enums.h>
+#include <getfiles.h>
+#include <structures.h>
+
+
+gchar ** get_files(gchar *subdir)
+{
+}
