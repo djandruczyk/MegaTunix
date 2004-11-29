b33e724161a24daa95de2f8e42d60f27accac760
extace <extace>
2004-11-29 22:58:17 +0000

    Fixed missing limit entries in the DT RealTimeMap files... NOTE: logviewer still has a hidden bug somplace...

--- src/logviewer_gui.c
+++ src/logviewer_gui.c
@@ -748,6 +748,7 @@ gboolean lv_configure_event(GtkWidget *widget, GdkEventConfigure *event, gpointe
 		gdk_window_clear(widget->window);
 	}
 
+	printf("configure event leaving\n");
 	return FALSE;
 }
 
