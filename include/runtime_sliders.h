1393e925faf030faa18ccf7e412d4270f7a0432b
extace <extace>
2004-12-19 02:11:08 +0000

    For some reasons runtime_sliders was missing from the repository...

---
+++ include/runtime_sliders.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#ifndef __RUNTIME_SLIDERS_H__
+#define __RUNTIME_SLIDERS_H__
+
+#include <enums.h>
+#include <gtk/gtk.h>
+#include <structures.h>
+
+/* Prototypes */
+void load_sliders(void );
+void add_slider(gchar *, gint, gint, gchar *,PageIdent );
+/* Prototypes */
+
+#endif
