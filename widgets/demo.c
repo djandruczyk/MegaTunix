f30ff79b0272ac2d65929eaa2c9e898f856c90e5
extace <extace>
2006-10-28 02:22:03 +0000

    Fixed xml creation order so that it self generates data

--- widgets/demo.c
+++ widgets/demo.c
@@ -60,8 +60,8 @@ int main (int argc, char **argv)
 	mtx_gauge_face_set_precision (MTX_GAUGE_FACE (gauge), 0);
 	gtk_timeout_add(20,(GtkFunction)update_gauge,(gpointer)gauge);
 
-	testload();
 	output_xml(MTX_GAUGE_FACE(gauge));
+	testload();
 	//gtk_widget_set_size_request(window,500,500);
 
 
