c3bdec39b693e243c685a86c17094d34e5b23697
extace <extace>
2006-12-29 20:10:05 +0000

    Rudimentary XML exporter semi-working...

--- dashdesigner/xml.c
+++ dashdesigner/xml.c
@@ -4,7 +4,12 @@
 #include <glib/gprintf.h>
 #include <gtk/gtk.h>
 #include <glade/glade.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 #include <xml.h>
+
+
+#if defined(LIBXML_TREE_ENABLED) && defined(LIBXML_OUTPUT_ENABLED)
 
 
 EXPORT gboolean import_dash_xml(GtkWidget *widget, gpointer data)
@@ -16,7 +21,77 @@ EXPORT gboolean import_dash_xml(GtkWidget *widget, gpointer data)
 
 EXPORT gboolean export_dash_xml_default(GtkWidget *widget, gpointer data)
 {
+	extern GladeXML *main_xml;
+	GtkWidget *dash = NULL;
+	GList *children = NULL;
+	GtkFixedChild *child = NULL;
+	gchar * tmpbuf = NULL;
+	gint i = 0;
+	xmlDocPtr doc = NULL;       /* document pointer */
+	xmlNodePtr root_node = NULL;/* node pointers */
+	xmlNodePtr node = NULL;/* node pointers */
+	xmlDtdPtr dtd = NULL;       /* DTD pointer */
+	gchar * filename = "testdash.xml";
+
+
 	printf("Export Dash XML default\n");
+
+	doc = xmlNewDoc(BAD_CAST "1.0");
+	root_node = xmlNewNode(NULL,BAD_CAST "dashboard");
+	xmlDocSetRootElement(doc,root_node);
+	/*
+	 * Creates a DTD declaration. Isn't mandatory. 
+	 */
+	dtd = xmlCreateIntSubset(doc, BAD_CAST "dashboard", NULL, BAD_CAST "mtxdashboard.dtd");
+
+	dash = glade_xml_get_widget(main_xml,"dashboard");
+
+	node = xmlNewChild(root_node,NULL,BAD_CAST "dash_geometry", NULL);
+	tmpbuf = g_strdup_printf("%i",dash->allocation.width);
+	xmlNewChild(node, NULL, BAD_CAST "width", BAD_CAST tmpbuf);
+	g_free(tmpbuf);
+	tmpbuf = g_strdup_printf("%i",dash->allocation.height);
+	xmlNewChild(node, NULL, BAD_CAST "height", BAD_CAST tmpbuf);
+	g_free(tmpbuf);
+
+	children = GTK_FIXED(dash)->children;
+	for(i=0;i<g_list_length(GTK_FIXED(dash)->children);i++)
+	{
+		child = g_list_nth_data(GTK_FIXED(dash)->children,i);
+		node = xmlNewChild(root_node,NULL,BAD_CAST "gauge", NULL);
+
+		tmpbuf = g_strdup_printf("%i",child->widget->allocation.width);
+		xmlNewChild(node, NULL, BAD_CAST "width", BAD_CAST tmpbuf);
+		tmpbuf = g_strdup_printf("%i",child->widget->allocation.height);
+		xmlNewChild(node, NULL, BAD_CAST "height", BAD_CAST tmpbuf);
+		g_free(tmpbuf);
+		tmpbuf = g_strdup_printf("%i",child->x);
+		xmlNewChild(node, NULL, BAD_CAST "x_offset", BAD_CAST tmpbuf);
+		g_free(tmpbuf);
+		tmpbuf = g_strdup_printf("%i",child->y);
+		xmlNewChild(node, NULL, BAD_CAST "y_offset", BAD_CAST tmpbuf);
+		g_free(tmpbuf);
+		tmpbuf = g_strdup_printf("%s",mtx_gauge_face_get_xml_filename(MTX_GAUGE_FACE(child->widget)));
+		xmlNewChild(node, NULL, BAD_CAST "gauge_xml_name", BAD_CAST tmpbuf);
+		g_free(tmpbuf);
+		printf("child %i\n",i);
+	}
+	xmlSaveFormatFileEnc(filename, doc, "utf-8", 1);
+
+	/*free the document */
+	xmlFreeDoc(doc);
+
+	/*
+	 *Free the global variables that may
+	 *have been allocated by the parser.
+	 */
+	xmlCleanupParser();
+
+	/*
+	 * this is to debug memory for regression tests
+	 */
+	xmlMemoryDump();
+
 	return TRUE;
 }
 
@@ -27,3 +102,4 @@ EXPORT gboolean export_dash_xml_as(GtkWidget *widget, gpointer data)
 	return TRUE;
 }
 
+#endif
