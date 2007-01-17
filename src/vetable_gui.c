b41dcf8f83239f5a3e4c25f7265f65a5a95e7983
extace <extace>
2007-01-17 22:54:15 +0000

    Major cool features.  highlighted 2D VEtable cells,  brightness corresponds to how close you are to that vertex...

--- src/vetable_gui.c
+++ src/vetable_gui.c
@@ -21,6 +21,7 @@
 #include <gui_handlers.h>
 #include "../mtxmatheval/mtxmatheval.h"
 #include <logviewer_gui.h>
+#include <math.h>
 #include <rtv_processor.h>
 #include <structures.h>
 #include <threads.h>
@@ -139,24 +140,32 @@ void draw_ve_marker()
 	gfloat x_source = 0.0;
 	gfloat y_source = 0.0;
 	GtkWidget *widget = NULL;
-	static GtkWidget *last_widget[20];
+	static GtkWidget ***last_widgets = NULL;
+	static gint **last = NULL;
+	static GdkColor ** old_colors = NULL;
+	static GdkColor color= { 0, 0,16384,16384};
 	GtkRcStyle *style = NULL;
-	static GdkColor old_color[20];
 	gint i = 0;
+	gint j = 0;
 	gint table = 0;
 	gint page = 0;
 	gint base = 0;
-	gint bin = 0;
-	gint x_bin = 0;
-	gint y_bin = 0;
-	gfloat tmp1 = 0.0;
-	gfloat tmp2 = 0.0;
-	gfloat midpoint = 0.0;
+	gint z_bin[4] = {0,0,0,0};
+	gint bin[4] = {0,0,0,0};
+	gfloat left_w = 0.0;
+	gfloat right_w = 0.0;
+	gfloat top_w = 0.0;
+	gfloat bottom_w = 0.0;
+	static gfloat last_z_weight[4] = {0,0,0,0};
+	gfloat z_weight[4] = {0,0,0,0};
+	gfloat left = 0.0;
+	gfloat right = 0.0;
+	gfloat top = 0.0;
+	gfloat bottom = 0.0;
 	GList *list = NULL;
 	extern struct Firmware_Details *firmware;
 	static void ***eval;
 	extern gint ** ms_data;
-	extern GdkColor red;
 	extern GList ***ve_widgets;
 	extern gint *algorithm;
 	extern gint active_table;
@@ -165,97 +174,189 @@ void draw_ve_marker()
 	if (!eval)
 		eval = g_new0(void **, firmware->total_tables);
 
-	table = active_table;
-//	for (table=0;table<firmware->total_tables;table++)
+	if (!last)
 	{
-		if (!eval[table])
-			eval[table] = g_new0(void *, 2);
-		if ((algorithm[table] == ALPHA_N) && (firmware->table_params[table]->an_x_source))
-			lookup_current_value(firmware->table_params[table]->an_x_source,&x_source);
-		else
-			lookup_current_value(firmware->table_params[table]->x_source,&x_source);
+		last = g_new0(gint *, firmware->total_tables);
+		for (i=0;i<firmware->total_tables;i++)
+			last[i] = g_new0(gint, 4);
+	}
+	if (!last_widgets)
+	{
+		last_widgets = g_new0(GtkWidget **, firmware->total_tables);
+		for (i=0;i<firmware->total_tables;i++)
+			last_widgets[i] = g_new0(GtkWidget *, firmware->table_params[i]->x_bincount * firmware->table_params[i]->y_bincount);
+	}
+	if (!old_colors)
+	{
+		old_colors = g_new0(GdkColor *, firmware->total_tables);
+		for (i=0;i<firmware->total_tables;i++)
+			old_colors[i] = g_new0(GdkColor , firmware->table_params[i]->x_bincount * firmware->table_params[i]->y_bincount);
+	}
 
-		if ((!eval[table][0]) && (firmware->table_params[table]->x_conv_expr))
+	table = active_table;
+	if (!eval[table])
+		eval[table] = g_new0(void *, 2);
+	if ((algorithm[table] == ALPHA_N) && (firmware->table_params[table]->an_x_source))
+		lookup_current_value(firmware->table_params[table]->an_x_source,&x_source);
+	else
+		lookup_current_value(firmware->table_params[table]->x_source,&x_source);
+
+	if ((!eval[table][0]) && (firmware->table_params[table]->x_conv_expr))
+	{
+		eval[table][0] = evaluator_create(firmware->table_params[table]->x_conv_expr);
+	}
+	if ((!eval[table][1]) && (firmware->table_params[table]->y_conv_expr))
+	{
+		eval[table][1] = evaluator_create(firmware->table_params[table]->y_conv_expr);
+	}
+
+	/* Find bin corresponding to current rpm  */
+	for (i=0;i<firmware->table_params[table]->x_bincount-1;i++)
+	{
+		page = firmware->table_params[table]->x_page;
+		base = firmware->table_params[table]->x_base;
+		if (evaluator_evaluate_x(eval[table][0],ms_data[page][base]) >= x_source)
 		{
-			eval[table][0] = evaluator_create(firmware->table_params[table]->x_conv_expr);
+			bin[0] = -1;
+			bin[1] = 0;
+			left_w = 1;
+			right_w = 1;
+			break;
 		}
-		if ((!eval[table][1]) && (firmware->table_params[table]->y_conv_expr))
+		left = evaluator_evaluate_x(eval[table][0],ms_data[page][base+i]);
+		right = evaluator_evaluate_x(eval[table][0],ms_data[page][base+i+1]);
+
+		if ((x_source > left) && (x_source <= right))
 		{
-			eval[table][1] = evaluator_create(firmware->table_params[table]->y_conv_expr);
-		}
+			bin[0] = i;
+			bin[1] = i+1;
 
-		/* Find bin corresponding to current rpm  */
-		for (i=0;i<firmware->table_params[table]->x_bincount-1;i++)
+			right_w = (float)(x_source-left)/(float)(right-left);
+			left_w = 1.0-right_w;
+			break;
+
+		}
+		if (x_source > right)
 		{
-			page = firmware->table_params[table]->x_page;
-			base = firmware->table_params[table]->x_base;
-			bin = firmware->table_params[table]->x_bincount-1;
-			if (evaluator_evaluate_x(eval[table][0],ms_data[page][base]) >= x_source)
-			{
-				bin = 0;
-				break;
-			}
-			tmp1 = evaluator_evaluate_x(eval[table][0],ms_data[page][base+i]);
-			tmp2 = evaluator_evaluate_x(eval[table][0],ms_data[page][base+i+1]);
-			midpoint = (tmp1+tmp2)/2.0;
-
-			if ((tmp1 < x_source) && (x_source <= midpoint))
-			{
-				bin = i;
-				break;
-			}
-			if ((x_source > midpoint) && (x_source <= tmp2))
-			{
-				bin = i+1;
-				break;
-			}
+			bin[0] = i+1;
+			bin[1] = -1;
+			left_w = 1;
+			right_w = 1;
 		}
-		x_bin = bin;
-		if ((algorithm[table] == ALPHA_N) && (firmware->table_params[table]->an_y_source))
-			lookup_current_value(firmware->table_params[table]->an_y_source,&y_source);
-		else
-			lookup_current_value(firmware->table_params[table]->y_source,&y_source);
-		for (i=0;i<firmware->table_params[table]->y_bincount-1;i++)
+	}
+//	printf("left bin %i, right bin %i, left_weight %f, right_weight %f\n",bin[0],bin[1],left_w,right_w);
+
+	if ((algorithm[table] == ALPHA_N) && (firmware->table_params[table]->an_y_source))
+		lookup_current_value(firmware->table_params[table]->an_y_source,&y_source);
+	else
+		lookup_current_value(firmware->table_params[table]->y_source,&y_source);
+	for (i=0;i<firmware->table_params[table]->y_bincount-1;i++)
+	{
+		page = firmware->table_params[table]->y_page;
+		base = firmware->table_params[table]->y_base;
+		if (evaluator_evaluate_x(eval[table][1],ms_data[page][base]) >= y_source)
 		{
-			page = firmware->table_params[table]->y_page;
-			base = firmware->table_params[table]->y_base;
-			bin = firmware->table_params[table]->y_bincount-1;
-			if (evaluator_evaluate_x(eval[table][1],ms_data[page][base]) >= y_source)
-			{
-				bin = 0;
-				break;
-			}
-			tmp1 = evaluator_evaluate_x(eval[table][1],ms_data[page][base+i]);
-			tmp2 = evaluator_evaluate_x(eval[table][1],ms_data[page][base+i+1]);
-			midpoint = (tmp1+tmp2)/2.0;
+			bin[2] = -1;
+			bin[3] = 0;
+			top_w = 1;
+			bottom_w = 1;
+			break;
+		}
+		bottom = evaluator_evaluate_x(eval[table][1],ms_data[page][base+i]);
+		top = evaluator_evaluate_x(eval[table][1],ms_data[page][base+i+1]);
 
-			if ((tmp1 < y_source) && (y_source <= midpoint))
+		if ((y_source > bottom) && (y_source <= top))
+		{
+			bin[2] = i;
+			bin[3] = i+1;
+
+			top_w = (float)(y_source-bottom)/(float)(top-bottom);
+			bottom_w = 1.0-top_w;
+			break;
+
+		}
+		if (y_source > top)
+		{
+			bin[2] = i+1;
+			bin[3] = -1;
+			top_w = 1;
+			bottom_w = 1;
+		}
+	}
+	z_weight[0] = left_w*bottom_w;
+	z_weight[1] = left_w*top_w;
+	z_weight[2] = right_w*bottom_w;
+	z_weight[3] = right_w*top_w;
+
+	/* Cheap hack to see if things changed, if not don't waste CPU time inside
+	 * of pango changing the color unnecessarily
+	 * */
+	for (i=0;i<4;i++)
+	{
+		if (fabs(z_weight[i]-last_z_weight[i]) > 0.03)
+			goto redraw;
+	}
+	for (i=0;i<4;i++)
+		last_z_weight[i] = z_weight[i];
+	return;
+
+redraw:
+//	printf("bottom bin %i, top bin %i, bottom_weight %f, top_weight %f\n",bin[2],bin[3],bottom_w,top_w);
+
+	if ((bin[0] == -1) || (bin[2] == -1))
+		z_bin[0] = -1;
+	else
+		z_bin[0] = firmware->table_params[table]->z_base+bin[0]+(bin[2]*firmware->table_params[table]->x_bincount);
+	if ((bin[0] == -1) || (bin[3] == -1))
+		z_bin[1] = -1;
+	else
+		z_bin[1] = firmware->table_params[table]->z_base+bin[0]+(bin[3]*firmware->table_params[table]->x_bincount);
+	if ((bin[1] == -1) || (bin[2] == -1))
+		z_bin[2] = -1;
+	else
+		z_bin[2]= firmware->table_params[table]->z_base+bin[1]+(bin[2]*firmware->table_params[table]->x_bincount);
+	if ((bin[1] == -1) || (bin[3] == -1))
+		z_bin[3] = -1;
+	else
+		z_bin[3]= firmware->table_params[table]->z_base+bin[1]+(bin[3]*firmware->table_params[table]->x_bincount);
+	for (i=0;i<4;i++)
+	{
+		for (j=0;j<4;j++)
+		{
+			if ((last[table][i] != z_bin[j] ) && (last_widgets[table][last[table][i]]))
 			{
-				bin = i;
-				break;
-			}
-			if ((y_source > midpoint) && (y_source <= tmp2))
-			{
-				bin = i+1;
-				break;
+				//for (k=0;i<4;k++)
+			//	{
+		//			if (z_bin[k] != last[table][i])
+		//			{
+//				printf("setting to normal coord %i\n",last[table][i]);
+				gtk_widget_modify_base(GTK_WIDGET(last_widgets[table][last[table][i]]),GTK_STATE_NORMAL,&old_colors[table][last[table][i]]);
+				last_widgets[table][last[table][i]] = NULL;
+				//	}
+				//}
 			}
 		}
-		y_bin = bin;
+	}
 
-		list = ve_widgets[firmware->table_params[table]->z_page][firmware->table_params[table]->z_base+x_bin+(y_bin*firmware->table_params[table]->x_bincount)];
+	for (i=0;i<4;i++)
+	{
+		if (z_bin[i] == -1)
+			continue;
+		list = ve_widgets[firmware->table_params[table]->z_page][z_bin[i]];
 		widget = g_list_nth_data(list,0);
 
-		/* Don't "over update" it */
-		if (widget == last_widget[table])
-			return;
-		if (last_widget[table])
-			gtk_widget_modify_base(GTK_WIDGET(last_widget[table]),GTK_STATE_NORMAL,&old_color[table]);
-		last_widget[table] = widget;
+
+		last_widgets[table][z_bin[i]] = widget;
+		last[table][i] = z_bin[i];
 
 		style = gtk_widget_get_modifier_style(widget);
 
-		old_color[table] = style->base[GTK_STATE_NORMAL];
-		gtk_widget_modify_base(GTK_WIDGET(widget),GTK_STATE_NORMAL,&red);
+		old_colors[table][z_bin[i]] = style->base[GTK_STATE_NORMAL];
+		color.red = z_weight[i]*32768 +32767;
+		color.green = z_weight[i]*32768 +32767;
+		color.blue = z_weight[i]*32768 +32767;
+		gtk_widget_modify_base(GTK_WIDGET(widget),GTK_STATE_NORMAL,&color);
+		last_z_weight[i] = z_weight[i];
 	}
 
 }
