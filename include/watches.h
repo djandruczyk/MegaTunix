9cd8a3210259f6bfe455db277e91afb5527421d7
extace <extace>
2009-01-19 00:24:44 +0000

    Added missing data

---
+++ include/watches.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/* Runtime Gui Structures */
+
+#ifndef __WATCHES_H__
+#define __WATCHES_H__
+
+#include <defines.h>
+#include <gtk/gtk.h>
+
+typedef enum
+{
+	SINGLE_BIT,
+	MULTI_BIT,
+	EXACT,
+	THRESHOLD,
+	RANGE,
+	WATCH_COUNT
+}WatchStyle;
+
+typedef struct _DataWatch DataWatch;
+
+struct _DataWatch
+{
+	guint32 id;
+	gint bit;
+	gint low;
+	gint high;
+	gint exact;
+	gint threshold;
+	WatchStyle style;
+	gboolean state;
+	gchar * function;
+	gchar * varname;
+};
+/* Prototypes */
+EXPORT void fire_off_rtv_watches_pf();
+guint32 create_bit_watch(gchar *, gint, gint, gchar *);
+void watch_destroy(gpointer);
+void process_watches(gpointer, gpointer, gpointer);
+/* Prototypes */
+
+#endif
