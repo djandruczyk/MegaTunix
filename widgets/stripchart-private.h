037cd9a2b1d95824c508c217cbd4cf844e91b892
extace <extace>
2010-01-17 22:06:29 +0000

    Beginning skeleton code for stripchart widget, which will be used to redesign the logviewer and other tools.

---
+++ widgets/stripchart-private.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2006 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * MegaTunix stripchart widget
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ * This is a the PRIVATE implementation header file for INTERNAL functions
+ * of the widget.  Public functions as well the the gauge structure are 
+ * defined in the gauge.h header file
+ *
+ */
+
+#ifndef __STRIPCHART_PRIVATE_H__
+#define __STRIPCHART_PRIVATE_H__
+
+#include <gtk/gtk.h>
+#include <stripchart.h>
+
+#define MTX_STRIPCHART_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), MTX_TYPE_STRIPCHART, MtxStripChartPrivate))
+
+typedef struct _MtxStripChartPrivate      MtxStripChartPrivate;
+
+struct _MtxStripChartPrivate
+{
+        GdkPixmap *pixmap;      /*! Update/backing pixmap */
+        GdkPixmap *bg_pixmap;   /*! Static rarely changing pixmap */
+	gfloat *current;	/*! Array of current values */
+        gint w;                 /* Width of full widget */
+        gint h;                 /* Height of full widget */
+        gchar *value_font;      /* Font string for value */
+        gfloat value_font_scale;/* Font scale */
+        gchar *valname;         /* Value text to the let of the number */
+        cairo_t *cr;            /*! Cairo context,  not sure if this is good
+                                   too hold onto or not */
+        cairo_font_options_t * font_options;
+        GdkGC * gc;             /*! Graphics Context for drawing */
+        GdkColormap *colormap;  /*! Colormap for GC's */
+        GdkColor colors[NUM_COLORS];
+
+};
+
+
+gboolean mtx_stripchart_configure (GtkWidget *, GdkEventConfigure *);
+gboolean mtx_stripchart_expose (GtkWidget *, GdkEventExpose *);
+/* Not needed yet
+* gboolean mtx_stripchart_button_press (GtkWidget *,GdkEventButton *);
+* gboolean mtx_stripchart_motion_event (GtkWidget *,GdkEventMotion *);
+*/
+void mtx_stripchart_size_request (GtkWidget *, GtkRequisition *);
+void mtx_stripchart_class_init (MtxStripChartClass *class_name);
+void mtx_stripchart_init (MtxStripChart *gauge);
+gboolean mtx_stripchart_button_release (GtkWidget *,GdkEventButton *);
+void generate_stripchart_background(MtxStripChart *);
+void update_stripchart_position (MtxStripChart *);
+void mtx_stripchart_init_colors(MtxStripChart *);
+void mtx_stripchart_redraw (MtxStripChart *gauge);
+
+
+#endif
---
+++ widgets/stripchart.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2010 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * MegaTunix stripchart widget
+ * Inspired by Phil Tobin's MegaLogViewer
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+
+#include <config.h>
+#include <cairo/cairo.h>
+#include <stripchart.h>
+#include <stripchart-private.h>
+#include <gtk/gtk.h>
+
+
+/*!
+ \brief gets called when  a user wants a new stripchart
+ \returns a pointer to a newly created stripchart widget
+ */
+GtkWidget *mtx_stripchart_new ()
+{
+	return GTK_WIDGET (g_object_new (MTX_TYPE_STRIPCHART, NULL));
+}
+
+
+/*!
+ \brief gets the current value 
+ \param stripchart (MtxStripChart *) pointer to stripchart
+ */
+gboolean mtx_stripchart_get_values (MtxStripChart *stripchart, gfloat *values)
+{
+	gint i = 0;
+	MtxStripChartPrivate *priv = MTX_STRIPCHART_GET_PRIVATE(stripchart);
+	g_return_val_if_fail ((MTX_IS_STRIPCHART (stripchart)),FALSE);
+	for (i=0;i<priv->num_traces;i++)
+		if (values[i])
+			*values[i]=priv->current[i];
+	return TRUE;
+}
+
+
+/*!
+ \brief sets the current value 
+ \param stripchart (MtxStripChart *) pointer to stripchart
+ \param value (gfloat) new value
+ */
+void mtx_stripchart_set_values (MtxStripChart *stripchart, gfloat* values)
+{
+	gint i = 0;
+	MtxStripChartPrivate *priv = MTX_STRIPCHART_GET_PRIVATE(stripchart);
+	g_return_if_fail (MTX_IS_STRIPCHART (stripchart));
+	g_object_freeze_notify (G_OBJECT (stripchart));
+	for (i=0;i<priv->num_traces;i++)
+		if (values[i])
+			priv->current[i] = *values[i];
+	g_object_thaw_notify (G_OBJECT (stripchart));
+	mtx_stripchart_redraw(stripchart);
+}
+
+
+/*!
+ \brief Adds an additional trace
+ \param stripchart (MtxStripChart *) pointer to stripchart
+ \param value (gfloat) new value
+ */
+gint mtx_stripchart_add_trace(MtxStripChart *, gfloat min, gfloat max, GdkColor color)
+{
+	gint i = 0;
+	MtxStripChartPrivate *priv = MTX_STRIPCHART_GET_PRIVATE(stripchart);
+	g_return_if_fail (MTX_IS_STRIPCHART (stripchart));
+	g_object_freeze_notify (G_OBJECT (stripchart));
+	
+	/* add a trace.. */
+
+	g_object_thaw_notify (G_OBJECT (stripchart));
+	mtx_stripchart_redraw(stripchart);
+}
+
+
+
+
+/*!
+ \brief Removes trace per specifed index
+ \param stripchart (MtxStripChart *) pointer to stripchart
+ \param value (gfloat) new value
+ */
+void mtx_stripchart_delete_trace(MtxStripChart *, gint index)
+{}
