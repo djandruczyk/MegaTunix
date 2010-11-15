34bcc3f3075c99096ada65fc960e3cd7f43d1ca1
David Andruczyk <djandruczyk@yahoo.com>
2010-11-14 20:39:28 -0500

    Added jimstim.c

---
+++ src/jimstim.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#include <config.h>
+#include <debugging.h>
+#include <defines.h>
+#include <enums.h>
+#include <jimstim.h>
+
+
+extern gconstpointer *global_data;
+
+
+EXPORT gboolean jimstim_sweep_start(GtkWidget *widget, gpointer data)
+{
+	printf("jimstim sweep start not written yet\n");
+	return TRUE;
+}
+
+
+EXPORT gboolean jimstim_sweep_end(GtkWidget *widget, gpointer data)
+{
+	printf("jimstim sweep end not written yet\n");
+	return TRUE;
+}
