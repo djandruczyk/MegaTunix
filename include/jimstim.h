d48232ef76aeb3a94088be995b7fe6f8a8d40f15
David Andruczyk <djandruczyk@yahoo.com>
2010-11-14 20:36:16 -0500

    Added jimstim header

---
+++ include/jimstim.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#ifndef __JIMSTIM_H__
+#define __JIMSTIM_H__
+
+#include <gtk/gtk.h>
+#include <gtk/gtk.h>
+
+/* Prototypes */
+gboolean jimstim_sweep_start(GtkWidget *, gpointer);
+gboolean jimstim_sweep_end(GtkWidget *, gpointer);
+/* Prototypes */
+
+#endif
