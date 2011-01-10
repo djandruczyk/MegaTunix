0d856cec47a6e910b4f9e87ef94e7dd319aa7f63
David Andruczyk <djandruczyk@yahoo.com>
2011-01-09 21:38:40 -0500

    Added missing files

---
+++ src/plugins/mscommon/vetable_gui.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#ifndef __VETABLE_GUI_H__
+#define __VETABLE_GUI_H__
+
+#include <gtk/gtk.h>
+#include <enums.h>
+
+/* Prototypes */
+
+void common_draw_ve_marker(void );
+
+/* Prototypes */
+
+#endif
