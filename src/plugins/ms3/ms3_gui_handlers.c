7e8299761c03244e646d297d6de12b90aea87e5f
David Andruczyk <djandruczyk@yahoo.com>
2011-01-13 12:43:06 -0500

    Added missing README files and ms3 plugin skeleton code

---
+++ src/plugins/ms3/ms3_gui_handlers.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2003 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+#include <config.h>
+#include <debugging.h>
+#include <defines.h>
+#include <firmware.h>
+#include <ms3_plugin.h>
+#include <ms3_gui_handlers.h>
+#include <glade/glade.h>
+#include <gtk/gtk.h>
+
+
+extern gconstpointer *global_data;
+
+G_MODULE_EXPORT void ecu_gui_init(void)
+{
+	/* We have nothing to init, just return */
+	return;
+}
+
+
+
+G_MODULE_EXPORT gboolean ecu_button_handler(GtkWidget *widget, gpointer data)
+{
+	return TRUE;
+}
+
+
+
+G_MODULE_EXPORT gboolean ecu_bitmask_button_handler(GtkWidget *widget, gpointer data)
+{
+	return TRUE;
+}
+
+
+G_MODULE_EXPORT gboolean ecu_spin_button_handler(GtkWidget *widget, gpointer data)
+{
+	return TRUE;
+}
+
+
+G_MODULE_EXPORT gboolean ecu_sntry_handler(GtkWidget *widget, gpointer data)
+{
+	return TRUE;
+}
+
+
