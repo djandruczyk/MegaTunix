f93c2ada13d8892040f7cdaef87ac0128c20a69f
David Andruczyk <dandruczyk@gmail.com>
2011-08-26 12:51:43 -0400

    Added freeems benchtest boilerplate functions

---
+++ src/plugins/freeems/freeems_benchtest.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2002-2011 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/*!
+  \file src/plugins/freeems/freeems_benchtest.c
+  \ingroup FreeEMSPlugin,Plugins
+  \brief FreeEMS specific benchtest helper functions
+  \author David Andruczyk
+  */
+
+#include <freeems_benchtest.h>
+#include <gtk/gtk.h>
+
+
+/*!
+  \brief Validates the on screen data, prompting if necessary and creating
+  the packet for the ECU and sending it on it's way
+  */
+G_MODULE_EXPORT void benchtest_validate_and_run(void)
+{
+	printf("Not written yet\n");
+}
---
+++ src/plugins/freeems/freeems_benchtest.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2002-2011 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/*!
+  \file src/plugins/freeems/freeems_benchtest.h
+  \ingroup FreeEMSPlugin,Headers
+  \brief FreeEMS Bench Test functions
+  \author David Andruczyk
+  */
+
+#ifndef __FREEEMS_BENCHTEST_H__
+#define __FREEEMS_BENCHTEST_H__
+
+
+/* Prototypes */
+void benchtest_validate_and_run(void);
+/* Prototypes */
+
+#endif
