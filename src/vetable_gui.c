645a16e14d9391c5b01671db9018bea65edce433
David Andruczyk <dandruczyk@gmail.com>
2011-09-14 20:25:43 -0400

    Got lambdatable gauge update function being called..

--- src/vetable_gui.c
+++ src/vetable_gui.c
@@ -155,7 +155,6 @@ G_MODULE_EXPORT gfloat rescale(gfloat input, ScaleOp scaleop, gfloat factor)
 G_MODULE_EXPORT void draw_ve_marker(void)
 {
 	static void (*common_draw_ve_marker)(void) = NULL;
-	printf("draw_ve_marker!!\n");
 
 	if (!common_draw_ve_marker)
 		get_symbol("common_draw_ve_marker",(void *)&common_draw_ve_marker);
