aa9e0f086a63aca077f0d4adcd41eb9e5dc93473
David Andruczyk <dandruczyk@gmail.com>
2011-12-04 16:54:09 -0500

    Forgot to add missing file...

---
+++ include/plugindefines.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2002-2011 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/*!
+  \file include/plugindefines.h
+  \ingroup Headers
+  \brief defines for plugins
+  \author David Andruczyk
+  */
+
+#ifndef __PLUGINDEFINES_H__
+#define __PLUGINDEFINES_H__
+
+
+#define IN_PLUGIN 1
+
+#endif
