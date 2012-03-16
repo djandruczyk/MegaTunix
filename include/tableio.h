da014bbb617a07d1aa7740c2a0a060a7035188d3
David Andruczyk <dandruczyk@gmail.com>
2012-03-16 17:41:20 -0400

    Added stub files for yaml table input/ouput functionality

---
+++ include/tableio.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2002-2012 by Dave J. Andruczyk <djandruczyk at yahoo dot com>
+ *
+ * Linux Megasquirt tuning software
+ * 
+ * 
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute, etc. this as long as all the source code
+ * is made available for FREE.
+ * 
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/*!
+  \file include/tableio.h
+  \ingroup Headers
+  \brief tableio header
+  \author David Andruczyk
+  */
+
+#ifndef __TABLEIO_H__
+#define __TABLEIO_H__
+
+#include <yaml-cpp/yaml.h>
+
+/* Prototypes */
+/* Prototypes */
+
+#endif
