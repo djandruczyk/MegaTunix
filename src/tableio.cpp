deecd80603652a19406e57636b4dce9d78337fbb
David Andruczyk <dandruczyk@gmail.com>
2012-03-16 22:30:54 -0400

    90% there.  ECU plugin stub function needs fleshing out to populate the data.  This is harder becasue fred insists on using user visible data which requires individula conversion and processing vs the raw ecu data

--- src/tableio.cpp
+++ src/tableio.cpp
@@ -29,6 +29,7 @@ extern "C" {
 #include <firmware.h>
 #include <gtk/gtk.h>
 #include <notifications.h>
+#include <plugin.h>
 #include <tableio.h>
 #include <widgetmgmt.h>
 }
@@ -116,15 +117,23 @@ void operator >> (const YAML::Node &node, ThreeDTable &tbl) {
 
 /* This doesn't work yet */
 G_MODULE_EXPORT void import_single_table(gint table_num) {
+	gboolean error = FALSE;
+	void (*ecu_table_import)(gint, gfloat *, gfloat *, gfloat *) = NULL;
+	gfloat *x_elements = NULL;
+	gfloat *y_elements = NULL;
+	gfloat *z_elements = NULL;
 	Firmware_Details *firmware = NULL;
 	MtxFileIO *fileio = NULL;
 	ThreeDTable *tbl = NULL;
 	gchar *filename = NULL;
+	gint x_len,y_len,z_len;
+	gint x_cur,y_cur,z_cur;
 
 	firmware = (Firmware_Details *)DATA_GET(global_data,"firmware");
+	get_symbol("ecu_table_import",(void **)&ecu_table_import);
 	g_return_if_fail(firmware);
+	g_return_if_fail(ecu_table_import);
 	g_return_if_fail(DATA_GET(global_data,"interrogated"));
-	printf("table_num is %i, total tables %i\n",table_num,firmware->total_tables);
 	g_return_if_fail(((table_num >= 0) && (table_num < firmware->total_tables)));
 
 	fileio = g_new0(MtxFileIO ,1);
@@ -136,11 +145,51 @@ G_MODULE_EXPORT void import_single_table(gint table_num) {
 	filename = choose_file(fileio);
 	if (!filename)
 	{
-		update_logbar("tools_view","warning",_("NO FILE chosen for VEX import\n"),FALSE,FALSE,FALSE);
+		update_logbar("tools_view","warning",_("NO FILE chosen for Table import\n"),FALSE,FALSE,FALSE);
 		return;
 	}
 	tbl = yaml_3d_import(filename);
 	/* Need to validate the axis ddimensions and table size */
+	x_len = tbl->X.num_elements;
+	y_len = tbl->Y.num_elements;
+	z_len = tbl->Z.values.size();
+	x_cur = firmware->table_params[table_num]->x_bincount;
+	y_cur = firmware->table_params[table_num]->y_bincount;
+	z_cur = firmware->table_params[table_num]->x_bincount * firmware->table_params[table_num]->y_bincount;
+	/*
+	printf("Import X size %i, Y size %i, Z length %i\n",x_len,y_len,z_len);
+	printf("Firmware table x size is %i, y size is %i, Z length is %i\n",x_cur,y_cur,z_cur);
+	*/
+	if (x_len != x_cur) {
+		printf("X axis lengths (%i != %i) do NOT MATCH!\n",x_len,x_cur);
+		error = TRUE;
+	}
+	if (y_len != y_cur) {
+		printf("Y axis lengths (%i != %i) do NOT MATCH!\n",y_len,y_cur);
+		error = TRUE;
+	}
+	if (z_len != z_cur) {
+		printf("Z cellcounts (%i != %i) do NOT MATCH!\n",z_len,z_cur);
+		error = TRUE;
+	}
+	if (error)
+		printf("Proposed file %s\nDOES NOT match up with running firmware,aborting load...\n",filename);
+	else
+	{
+		x_elements = g_new0(gfloat, z_len);
+		for (unsigned i=0;i<tbl->X.values.size();i++)
+			x_elements[i] = tbl->X.values[i];
+		y_elements = g_new0(gfloat, z_len);
+		for (unsigned i=0;i<tbl->Y.values.size();i++)
+			y_elements[i] = tbl->Y.values[i];
+		z_elements = g_new0(gfloat, z_len);
+		for (unsigned i=0;i<tbl->Z.values.size();i++)
+			z_elements[i] = tbl->Z.values[i];
+		ecu_table_import(table_num,x_elements,y_elements,z_elements);
+	}
+	g_free(filename);
+			
+	delete tbl;
 }
 
 ThreeDTable * yaml_3d_import(gchar * filename) {
